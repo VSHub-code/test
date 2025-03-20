a8aeeb3268d706c64c6fdae011915044ae328eb1
VSHub-code <vadilah797@gmail.com>
2025-03-20 13:13:31 +0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main () {
+    string name;
+    cout <<"siapa namamu?/n";
+    cin >>"nama/n";
+    cout <<"halo" << nama << ( "!/n");
+    
+    return 0;
+}
